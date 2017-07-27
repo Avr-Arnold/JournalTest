f06020984bbfa4f8be64d3fd0a90d8909c5fd1be
Avi <Avi@LAPTOP-9OM83AHS>
2017-07-27 16:40:25 -0400

    init

---
+++ Journal1/OneTimeUses/Paper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+
+namespace OneTimeUses
+{
+    class Paper
+    {
+	    public ObjectId Id { get; set; }
+	    public string Fields { get; set; } //I would prefer Enums for most, but get it working first
+	    public string PsychArea { get; set; }
+	    public string KeyTopic { get; set; }
+	    public string TargetPopulation { get; set; }
+	    public int Number { get; set; }
+	    public string Title { get; set; }
+	    public List<string> Authors { get; set; }
+	    public string JournalName { get; set; }
+	    public string AlertMonth { get; set; }
+	    public string PublicationDate { get; set; }
+	    public string Link { get; set; }
+	    public string Abstract { get; set; }
+	}
+}
---
+++ Journal1/OneTimeUses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace OneTimeUses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			BsonClassMap.RegisterClassMap<Paper>();
+			var client = new MongoClient("mongodb://localhost:27017");
+			//using (var cursor = client.ListDatabases())
+			//{
+			//	foreach (var document in cursor.ToEnumerable())
+			//	{
+			//		Console.WriteLine(document.ToString());
+			//	}
+			//}
+			var database = client.GetDatabase("Papers");
+			var collection = database.GetCollection<Paper>("bar");
+			
+
+			
+			Console.WriteLine("Hello World!");
+	        Console.Read();
+        }
+    }
+
+}
